b21b78b407b41f1332d3758ee659aef16ed1d52b
4ammarwat <108218885+4ammarwat@users.noreply.github.com>
2022-06-26 16:15:16 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,117 @@
+
+<!DOCTYPE html
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+  <title>OFFICIAL</title>
+</head>
+
+<body>
+  <i class="uil uil-angle-up up"></i>
+  <header>
+    <h2>..</h2>
+    <ul>
+      <li><a href="#home" id="nav1">Home</a></li>
+      <li><a href="#s" id="nav2"></a></li>
+      <li><a href="kills" id="nav3">Skills</a></li>
+      <li><a href="#contact" id="nav4">Contact</a></li>
+      <li>
+        <div class="switch">
+          <div class="switchcircle">
+            <ion-icon name="moon-outline"></ion-icon>
+          </div>
+        </div>
+      </li>
+    </ul>
+    <i class="uil uil-bars menu"></i>
+  </header>
+  <section id="home">
+    <div class="left">
+      <span>Hello,i'm malak sadam badsha</span>
+      <h3>PTI WORKER</h3>
+      <span class="des">i am also chief of battu khel.</span>
+      <button><a href="#skills">click</a></button>
+    </div>
+    <div class="right">
+      <img src="portimg.svg">
+    </div>
+    <h1 class="bgtitle">OFFICIAL WEBSITE Of malik sadam badsha..</h1>
+    <h1 class="ya">YA</h1>
+    <div class="circle1"></div>
+    <div class="circle2"></div>
+    <div class="circle3"></div>
+  </section>
+  <section id="">
+    <h1 class="title"></h1>
+    <div class="cards">
+      <div class="card">
+        <div class="bgcard"></div>
+        <ion-icon name="" class="html"></ion-icon>
+        <h3></h3>
+        <p><br></p>
+      </div>
+      <div class="card">
+        <div class="bgcard"></div>
+        <ion-icon name="logo-PTi" class="css"></ion-icon>
+        <h3></h3>
+        <p><br>.</p>
+      </div>
+      <div class="card">
+        <div class="bgcard"></div>
+        
+        <p></p>
+      </div>
+    </div>
+  </section>
+  <section id="">
+    <h1 class="title"></h1>
+    <div class="content">
+      <div class="">
+        <div class="skill">
+          <span class="bar"></span><br>
+          <div class="bar"></div>
+          <div class="activebar" id="bar1"></div>
+        </div>
+        <div class="skill">
+          <span class="cssbar"></span><br>
+          <div class="bar"></div>
+          <div class="activebar" id="bar2"></div>
+        </div>
+        <div class="skill">
+          <span class="jsbar"></span><br>
+          <div class="bar"></div>
+          <div class="activebar" id="bar3"></div>
+        </div>
+      </div>
+      <div class="para">
+        <h3>!!</h3>
+        <p>i am from tajazai i do work hard for my peoples<br>And i trying to do alot of  things for my peoples.<br>insha'Allah i will work for pti and my people till to my last breath .</p>
+      </div>
+    </div>
+   
+  </section>
+  <section id="contact">
+    <h1 class="title">Contact</h1>
+    <div class="cards">
+      <div class="card">
+        <ion-icon name="call-outline"></ion-icon>
+        <h3>Phone</h3>
+        <p>03139000386</p>
+      </div>
+      <div class="card">
+        <ion-icon name="mail-outline"></ion-icon>
+        <h3>FACEBOOK PAGE</h3>
+        <p>Malik sadam badsha marwat </p>
+      </div>
+    </div>
+  </section>
+  <script src="script.js"></script>
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https: // unpkg .com / ionicons @ 5.5.2 / dist / ionicons / ionicons.js"></script>
+</body>
+
+</>
